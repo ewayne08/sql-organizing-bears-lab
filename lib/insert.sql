e46b6ca86368e53e4fa4088103e750b9ecabf99e
Ethan <ethanchristianwayne@gmail.com>
2020-11-16 19:35:54 -0500

    commit tree fa8cb47de220d07c10813d7644859bd4453e7475

---
+++ lib/insert.sql
@@ -0,0 +1,6 @@
+(11, "Ed", 8, "M", "dark brown", "gentle", "TRUE"), 
+(12, "Olie", 16, "M", "light brown", "goofy", "TRUE"), 
+(13, "Mickey", 10, "M", "dark brown", "dominant", "TRUE"), 
+(14, "Freckles", 5, "M", "dark brown", "territorial", "FALSE"), 
+(15, "Quincy", 26, "M", "auburn", "gentle", "TRUE"),
+(16, NULL, 20, "M", "dark brown", "agressive", "FALSE");
